afc205c549908945a0cdd2c46751f925c972ac02
Igor.Sokolov <I.Sokolov@ya.ru>
2022-05-05 20:49:58 +0400

    add templates for set arrays and get properties

--- EngineEx_Template.cs
+++ EngineEx_Template.cs
@@ -14,7 +14,7 @@ namespace Engine
         /// <summary>
         /// 
         /// </summary>
-        private Int64 m_instance = 0;
+        protected Int64 m_instance = 0;
 
         /// <summary>
         /// 
@@ -74,10 +74,34 @@ namespace Engine
         /// </summary>
         /// <param name="name"></param>
         /// <param name="value"></param>
+        public void SetDatatypeProperty(string name, double[] values)
+        {
+            var propId = GetPropertyId(name);
+            var res = x86_64.SetDataTypeProperty(m_instance, propId, values, values.Length);
+            System.Diagnostics.Debug.Assert(res == 0);
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="name"></param>
+        /// <param name="value"></param>
         public void SetDatatypeProperty(string name, long value)
         {
             var propId = GetPropertyId(name);
             var res = x86_64.SetDataTypeProperty(m_instance, propId, ref value, 1);
+            System.Diagnostics.Debug.Assert(res == 0);
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="name"></param>
+        /// <param name="values"></param>
+        public void SetDatatypeProperty(string name, long[] values)
+        {
+            var propId = GetPropertyId(name);
+            var res = x86_64.SetDataTypeProperty(m_instance, propId, values, values.Length);
             System.Diagnostics.Debug.Assert(res == 0);
         }
 
@@ -98,11 +122,39 @@ namespace Engine
         /// 
         /// </summary>
         /// <param name="name"></param>
+        /// <param name="values"></param>
+        public void SetDatatypeProperty(string name, bool[] values)
+        {
+            byte[] bytes = new byte[values.Length];
+            for (int i = 0; i < values.Length; i++)
+                bytes[i] = values[i] ? (byte)1 : (byte)0;
+            
+            var propId = GetPropertyId(name);
+            var res = x86_64.SetDataTypeProperty(m_instance, propId, bytes, values.Length);
+            System.Diagnostics.Debug.Assert(res == 0);
+        }
+
+        /// <summary>
+        /// 1111
+        /// </summary>
+        /// <param name="name"></param>
         /// <param name="value"></param>
         public void SetDatatypeProperty(string name, string value)
         {
             var propId = GetPropertyId(name);
             var res = x86_64.SetDataTypeProperty(m_instance, propId, ref value, 1);
+            System.Diagnostics.Debug.Assert(res == 0);
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="name"></param>
+        /// <param name="value"></param>
+        public void SetDatatypeProperty(string name, string[] values)
+        {
+            var propId = GetPropertyId(name);
+            var res = x86_64.SetDataTypeProperty(m_instance, propId, values, values.Length);
             System.Diagnostics.Debug.Assert(res == 0);
         }
 
@@ -244,13 +296,13 @@ namespace Engine
             var res = x86_64.SetObjectProperty(m_instance, propId, ref instance, 1);
             System.Diagnostics.Debug.Assert(res == 0);
         }
-
     }
-//## TEMPLATE: BeginWrapperClass
-/// <summary>
-/// 
-/// </summary>
-public class CLASS_NAME : /*BASE CLASS*/Instance
+
+    //## TEMPLATE: BeginWrapperClass
+    /// <summary>
+    /// 
+    /// </summary>
+    public class CLASS_NAME : /*BASE CLASS*/Instance
     {
         public CLASS_NAME(Int64 instance, string chekClassName = null) 
             : base (instance, (chekClassName!=null) ? chekClassName : "CLASS_NAME") 
@@ -262,30 +314,66 @@ public class CLASS_NAME : /*BASE CLASS*/Instance
        //
 //## TEMPLATE: SetDataProperty
         public void set_PROPERTY_NAME(double value) { SetDatatypeProperty ("PROPERTY_NAME", value); }
+//## TEMPLATE SetDataArrayProperty
+        public void set_PROPERTY_NAME(double[] values) { SetDatatypeProperty ("PROPERTY_NAME", values); }
+//## TEMPLATE GetDataProperty
         public double[] get_PROPERTY_NAME() { return GetDatatypeProperty_double("PROPERTY_NAME"); }
-
 //## TEMPLATE: SetObjectProperty
         public void set_PROPERTY_NAME(Instance instance) { SetObjectProperty("PROPERTY_NAME", instance); }
-        
-//## TEMPLATE: EndWrapperClass
-    }
-//## TEMPLATE - BeginFactoryClass
+//## TEMPLATE SetObjectArrayProperty
+        public void SetObjectProperty(string name, Instance[] instances)
+        {
+            var propId = GetPropertyId(name);
 
+            var inst = new Int64[instances.Length];
+            for (int i=0; i<instances.Length;i++)
+                inst[i]=instances[i];
+
+            var res = x86_64.SetObjectProperty(m_instance, propId, ref inst[0], inst.Length);
+            System.Diagnostics.Debug.Assert(res == 0);
+        }
+//## TEMPLATE GetObjectProperty
+        public Instance[] get_PROPERTY_NAME_Instance() 
+        {
+            var propId = GetPropertyId("PROPERTY_NAME");
+
+            Int64 card = 0;
+            IntPtr valuesPtr = IntPtr.Zero;
+            var res = Engine.x86_64.GetDatatypeProperty(m_instance, propId, out valuesPtr, out card);
+            System.Diagnostics.Debug.Assert(res == 0);
+
+            if (card > 0)
+            {
+                var values = new Int64[card];
+                System.Runtime.InteropServices.Marshal.Copy(valuesPtr, values, 0, (int)card);
+
+                var ret = new Instance[card];
+                for (int i = 0; i < card; i++)
+                {
+                    ret[i] = new Instance(values[i], null);
+                }
+
+                return ret;
+            }
+            else
+            {
+                return null;
+            }
+        }
+        //## TEMPLATE: EndWrapperClass
+    }
+
+    //## TEMPLATE - BeginFactoryClass
     /// <summary>
     /// Factory class to create instances
     /// </summary>
     public class Create
     {
 //## FactoryMethod template part
-        /// <summary> 
-        /// Create instance of CLASS_NAME
-        /// </summary>
-        /// <param name="model">The handle to the model</param>
-        /// <param name="name">Name of the instance (optional)</param>
-        /// <returns>Returns a handle to created instance</returns>
+        /// <summary>Create instance of CLASS_NAME</summary>
         public static CLASS_NAME CLASS_NAME(Int64 model, string name=null) { return new CLASS_NAME (CreateInstance(model, "CLASS_NAME", name));}
-
 //## EndFile template part
+
         private static Int64 CreateInstance(Int64 model, string className, string instanseName)
         {
             Int64 clsid = x86_64.GetClassByName(model, className);
