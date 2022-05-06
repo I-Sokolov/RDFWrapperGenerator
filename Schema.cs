512ff0e3fae99d83fed61c2641c51d206a942e65
Igor.Sokolov <I.Sokolov@ya.ru>
2022-05-06 18:28:26 +0400

    looking for examples of bool and sring properties with cardinality > 1

--- Schema.cs
+++ Schema.cs
@@ -113,6 +113,10 @@ namespace RDFWrappers
                     clsprop.min = min;
                     clsprop.max = max;
 
+                    //looking for examples of bool and sring properties with cardinality > 1
+                    System.Diagnostics.Debug.Assert(prop.Value.DataType() != "bool" || clsprop.max == 1);
+                    System.Diagnostics.Debug.Assert(prop.Value.DataType() != "string" || clsprop.max == 1);
+
                     cls.properties.Add(clsprop);
                 }
             }
