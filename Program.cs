40ba31bfd58412444a4c7bacd45a8c738e699bee
Igor.Sokolov <I.Sokolov@ya.ru>
2022-07-07 11:54:54 +0400

    options reworks - generate for all embedded schemas

--- Program.cs
+++ Program.cs
@@ -8,19 +8,22 @@ namespace RDFWrappers
     {
         public class Options
         {
-            [CommandLine.Option("schema", HelpText = "Pathname of EXPRESS file or name of embedded schema.")]
+            [CommandLine.Option("embedded", Default = false, HelpText = "Generate wrapper classes for all embedded schemas (IFC2x3, IFC4, IFC4x4, CIS2, AP242)")]
+            public bool embedded { get; set; }
+
+            [CommandLine.Option("schema", HelpText = "Generate wrapper for by EXPRESS file path or schema name")]
             public string schema { get; set; }
 
-            [CommandLine.Option("cs", HelpText = "Pathnamte of c# file to be generated. Default is model name.")]
+            [CommandLine.Option("cs", HelpText = "Pathnamte of c# file to be generated. Default is schema file name.")]
             public string csFile { get; set; }
 
-            [CommandLine.Option('h', HelpText = "Pathname of c++ header file to be generated. Default is model name.")]
+            [CommandLine.Option('h', HelpText = "Pathname of c++ header file to be generated. Default is schema file name.")]
             public string hFile { get; set; }
 
-            [CommandLine.Option("namespace", HelpText = "Namespase. Default is schema name.")]
+            [CommandLine.Option("namespace", HelpText = "Namespase. Default is schema file name.")]
             public string Namespace { get; set; }
 
-            [CommandLine.Option("printSchema", Default = false, HelpText = "Print schema declarations.")]
+            [CommandLine.Option("printSchema", Default = false, HelpText = "Print parsed schema.")]
             public bool printSchema { get; set; }
         }
 
@@ -28,10 +31,7 @@ namespace RDFWrappers
         {
             try
             {
-                /*
-                var sdai = new ExpressSchema("IFC2x3");
-                sdai.ToConsole();
-                */
+                Console.WriteLine("This application generates C# and C++ header files with helper classes to work with EXPRESS models.");
 
                 Options options = null;
                 Parser.Default.ParseArguments<Options>(args).WithParsed<Options>(o => { options = o; });
@@ -40,96 +40,103 @@ namespace RDFWrappers
                     return -1;//--help option
                 }
 
-                //
-                Console.WriteLine("This application generates C# and C++ header files with helper classes to work with EXPRESS models.");
-                Console.WriteLine("Use --help to ptint more information and visit http://www.rdf.bg");
-                Console.WriteLine("");
+                bool done = false;
 
-                //
-                // Update and validate options
-                //
-                if (string.IsNullOrWhiteSpace (options.schema))
+                if (options.embedded)
                 {
-                    //options.schema = "ap242ed2_mim_lf_v1.101.exp";
-                    //options.schema = "..\\..\\..\\EXPRESS\\structural_frame_schema.exp";
-                    //options.schema = "..\\..\\..\\EXPRESS\\IFC2X3_TC1.exp";                    
-                    //options.schema = "..\\..\\..\\EXPRESS\\IFC4_ADD2_TC1.exp";
-                    //options.schema = "..\\..\\..\\EXPRESS\\IFC4x1.exp";
-                    //options.schema = "..\\..\\..\\EXPRESS\\IFC4x2.exp";
-                    //options.schema = "..\\..\\..\\EXPRESS\\IFC4x3.exp";
-                    //options.schema = "AP242";
-                    options.schema = "IFC4";
-                    //options.schema = "IFC2x3";
-                    //options.schema = "IFC4x3";
-                    //options.schema = "CIS2";
+                    GenerateForEmbedded();
+                    done = true;
                 }
 
-                string baseName = System.IO.Path.GetFileNameWithoutExtension (options.schema);
-
-                if (string.IsNullOrWhiteSpace (options.csFile))
+                if (!string.IsNullOrEmpty(options.schema))
                 {
-                    options.csFile = baseName + ".cs";
-                }
-                if (string.IsNullOrWhiteSpace (options.hFile))
-                {
-                    options.hFile = baseName + ".h";
-                }
-                if (string.IsNullOrWhiteSpace (options.Namespace))
-                {
-                    options.Namespace = baseName;
-                }
-                options.Namespace = Generator.ValidateIdentifier(options.Namespace);
-
-                //
-                // Main course
-                //
-                Console.WriteLine("Generating classes for " + baseName);
-
-                var schema = new ExpressSchema(options.schema);
-
-                Console.WriteLine("....");
-
-                if (options.printSchema)
-                {
-                    schema.ToConsole();
-                    Console.WriteLine();
+                    Generate(options);
+                    done = true;
                 }
 
                 //
-                /*/
-                if (!string.IsNullOrWhiteSpace(options.csFile))
+                if (!done)
                 {
-                    Console.WriteLine("Generate C# file " + options.csFile);
-                    Generator csgen = new Generator(schema, true, options.Namespace);
-                    csgen.WriteWrapper(options.csFile);
+                    Console.WriteLine("\nNothing generated.\nSpecify command line option --embedded or --schema.\n");
+                    Console.WriteLine("Use --help to ptint more information and visit http://www.rdf.bg.");
+                    return -1;
                 }
-                else
-                {
-                    Console.WriteLine("Do not create C# file");
-                }
-                System.Console.WriteLine();*/
-
-                if (!string.IsNullOrWhiteSpace(options.hFile))
-                {
-                    Console.WriteLine("Generate C++ header file " + options.hFile);
-                    Generator cppgen = new Generator(schema, false, options.Namespace);
-                    cppgen.WriteWrapper(options.hFile);
-                }
-                else
-                {
-                    Console.WriteLine("Do not create C++ header file");
-                }
-                System.Console.WriteLine();
 
                 return 0;
             }
             catch (Exception e)
             {
                 Console.WriteLine();
-                Console.WriteLine("ERROR:");
+                Console.WriteLine("EXCEPTION ERROR:");
                 Console.WriteLine(e.ToString());
                 return 13;
             }
+        }
+
+        private static void GenerateForEmbedded()
+        {
+            Generate("IFC2x3");
+            Generate("IFC4");
+            Generate("IFC4x3");
+            Generate("AP242");
+            Generate("CIS2");
+        }
+
+        private static void Generate (string schemaName)
+        {
+            var options = new Options();
+            options.schema = schemaName;
+
+            Generate(options);
+        }
+
+        private static void Generate(Options options)
+        {
+            Console.WriteLine("\n\nGenerating classes for " + options.schema);
+
+            string baseName = System.IO.Path.GetFileNameWithoutExtension(options.schema);
+
+            if (string.IsNullOrWhiteSpace(options.csFile))
+            {
+                options.csFile = baseName + ".cs";
+            }
+
+            if (string.IsNullOrWhiteSpace(options.hFile))
+            {
+                options.hFile = baseName + ".h";
+            }
+
+            if (string.IsNullOrWhiteSpace(options.Namespace))
+            {
+                options.Namespace = baseName;
+            }
+
+            options.Namespace = Generator.ValidateIdentifier(options.Namespace);
+
+            //
+            // Main course
+            //
+            var schema = new ExpressSchema(options.schema);
+
+            Console.WriteLine("....");
+
+            if (options.printSchema)
+            {
+                schema.ToConsole();
+                Console.WriteLine();
+            }
+
+            if (!string.IsNullOrWhiteSpace(options.hFile))
+            {
+                Console.WriteLine("Generate C++ header file " + options.hFile);
+                Generator cppgen = new Generator(schema, false, options.Namespace);
+                cppgen.WriteWrapper(options.hFile);
+            }
+            else
+            {
+                Console.WriteLine("Options says do not create C++ header file");
+            }
+            System.Console.WriteLine();
         }
     }
 }
